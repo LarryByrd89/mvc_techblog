b928c1d2a8e0c37553026453bb2e78682598c4aa
LarryByrd89 <laryn.n99@gmail.com>
2023-07-27 21:45:23 -0400

    user and pst models

---
+++ models/post.js
@@ -0,0 +1,44 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('');
+
+
+class Post extends Model {}
+// post model options
+Post.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        post_text: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+            validate: {
+                len: [1]
+            }
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user', 
+                key: 'id' 
+            }
+        }
+    },
+    {
+    //sequalize connection
+        sequelize,
+        freezeTableName: true, 
+        underscored: true, 
+        modelName: 'post' 
+    }
+);
+
+
+module.exports = Post;
